e5bfc285b8a2ba397a782d2a0da23a3dc733de54
Emmyojile <emmaojile99@gmail.com>
2023-11-04 00:07:26 -0800

    pushing to github

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,21 +1,22 @@
-"use client"
-import { CustomButtonProps } from '@/types'
-import React from 'react'
+"use client";
+import { CustomButtonProps } from "@/types";
+import React from "react";
 
-const CustomButton = ({title,containerStyles,handleClick}:CustomButtonProps) => {
+const CustomButton = ({
+  title,
+  containerStyles,
+  handleClick,
+}: CustomButtonProps) => {
   return (
     <button
-    disabled={false}
-    type={'button'}
-    className={`custom-btn ${containerStyles}`}
-    onClick={handleClick}
+      disabled={false}
+      type={"button"}
+      className={`custom-btn ${containerStyles}`}
+      onClick={handleClick}
     >
-        <span className={`flex-1`}>
-            {title}
-        </span>
-        
+      <span className={`flex-1`}>{title}</span>
     </button>
-  )
-}
+  );
+};
 
-export default CustomButton
+export default CustomButton;
