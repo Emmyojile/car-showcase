869cde4a815c088f4c9e72969bd4a47eb46e230a
Emmyojile <emmaojile99@gmail.com>
2023-11-05 15:54:01 -0800

    search funtion

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+"use client";
+
+import { SearchManufacturer } from ".";
 
 const SearchBar = () => {
+  const handleSearch = () => {};
   return (
-    <div>SearchBar</div>
-  )
-}
+    <form className="searchbar" onSubmit={handleSearch}>
+      <div className="searchbar__item">
+        <SearchManufacturer />
+      </div>
+    </form>
+  );
+};
 
-export default SearchBar
+export default SearchBar;
