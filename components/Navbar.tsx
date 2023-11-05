dccab31dd7a895a5539f101df9740b3538b34937
Emmyojile <emmaojile99@gmail.com>
2023-11-05 15:36:22 -0800

    layout

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,18 +1,29 @@
-import React from 'react'
+import React from "react";
 import Image from "next/image";
-import Link from 'next/link';
-
+import Link from "next/link";
+import { CustomButton } from ".";
 
 const Navbar = () => {
   return (
-    <header className='w-full absolute z-10'>
-        <nav>
-            <Link href="/">
-                <Image alt='Car-logo'/>
-            </Link>
-        </nav>
-    </header>
-  )
-}
+    <header className="w-full absolute z-10">
+      <nav className="max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-16 py-4">
+        <Link href="/" className="flex justify-center items-center">
+          <Image
+            src="/logo.svg"
+            alt="Car-logo"
+            width={118}
+            height={18}
+            className="object-contain"
+          />
+        </Link>
 
-export default Navbar
+        <CustomButton
+          title="Sign In"
+          containerStyles="text-primary-blue rounded-full bg-white min-w-[130px]"
+        />
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
